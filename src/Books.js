b86308201775cb3aa96f1b562623ce2377456b8c
Helon <rhelon@gmail.com>
2019-03-04 22:06:15 +0200

    Added styles and sort books button

--- src/Books.js
+++ src/Books.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import SearchArea from './SearchArea';
 import request from 'superagent';
+import BookList from './BookList';
 
 class Books extends Component {
     constructor(props) {
         super(props);
         this.state = {
             books: [],
-            searchField: ''
+            searchField: '',
+            sort: ''
         }
     }
 
@@ -17,7 +19,8 @@ class Books extends Component {
             .get("https://www.googleapis.com/books/v1/volumes")
             .query({ q: this.state.searchField })
             .then((data) => {
-            console.log(data)
+                const cleanData = this.cleanData(data)
+                this.setState({ books: cleanData })
         })
     }
 
@@ -25,10 +28,44 @@ class Books extends Component {
         this.setState({ searchField: e.target.value })
     }
 
+    handleSort = (e) => {
+        this.setState ({ sort: e.target.value })
+    }
+
+    cleanData = (data) => {
+        const cleanedData = data.body.items.map((book) => {
+            if(book.volumeInfo.hasOwnProperty('publishedDate') === false ) {
+                book.volumeInfo['publishedDate'] = '0000';
+            }
+
+            else if (book.volumeInfo.hasOwnProperty('imageLinks') === false) {
+                book.volumeInfo['imageLinks'] = { thumbnail: 'http://maestroselectronics.com/wp-content/uploads/2017/12/No_Image_Available.jpg'}
+            }
+
+            return book;
+        })
+
+        return cleanedData;
+    }
+
     render() {
+        const sortedBooks = this.state.books.sort((a, b) => {
+            if (this.state.sort === 'Newest') {
+                return parseInt(b.volumeInfo.publishedDate.substring(0, 4)) - parseInt(a.volumeInfo.publishedDate.substring(0, 4));
+            }
+            else if (this.state.sort === 'Oldest') {
+                return parseInt(a.volumeInfo.publishedDate.substring(0, 4)) - parseInt(b.volumeInfo.publishedDate.substring(0, 4));
+            }
+
+            return sortedBooks;
+        })
+
         return (
-            <div>
-                <SearchArea searchBook={this.searchBook} handleSearch={this.handleSearch}/>
+            <div className="wrapper">
+                <SearchArea 
+                    searchBook={this.searchBook}
+                    handleSearch={this.handleSearch} handleSort={this.handleSort}/>
+                <BookList books={sortedBooks}/>
             </div>
         );
     }
--- src/SearchArea.js
+++ src/SearchArea.js
@@ -3,9 +3,14 @@ import React from 'react';
 const SearchArea = (props) => {
     return (
         <div className="search-area">
-            <form onSubmit={props.searchBook} action="">
-            <input onChange={props.handleSearch} type="text"/>
+            <form onSubmit={props.searchBook}>
+                <input onChange={props.handleSearch} placeholder="Search books" type="text"/>
             <button type="submit">Search</button>
+                <select value={props.sort} onChange={props.handleSort}>
+                <option disabled value='Sort'>Sort</option>
+                <option value='Newest'>Newest</option>
+                <option value='Oldest'>Oldest</option>
+            </select>
             </form>
         </div>
     )
