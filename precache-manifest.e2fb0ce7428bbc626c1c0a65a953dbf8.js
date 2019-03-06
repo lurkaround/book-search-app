6d8a5339e2cdefa754620ed8d6befb34699ead3a
Helon <rhelon@gmail.com>
2019-03-06 11:24:48 +0200

    Updates

---
+++ precache-manifest.e2fb0ce7428bbc626c1c0a65a953dbf8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90699947a7a66c8bc55f",
+    "url": "/book-search-app/static/js/runtime~main.90699947.js"
+  },
+  {
+    "revision": "a8301dbac573566ca6c4",
+    "url": "/book-search-app/static/js/main.a8301dba.chunk.js"
+  },
+  {
+    "revision": "c27a4ea607343334a1b0",
+    "url": "/book-search-app/static/js/2.c27a4ea6.chunk.js"
+  },
+  {
+    "revision": "a8301dbac573566ca6c4",
+    "url": "/book-search-app/static/css/main.25f56a39.chunk.css"
+  },
+  {
+    "revision": "ed107a0b1e7adb37de7695f68888966f",
+    "url": "/book-search-app/index.html"
+  }
+];
